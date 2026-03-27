fe9177f9fdaff89a9fcc7179305d617958189974
Karl <kc0bfv@gmail.com>
2026-03-27 17:48:34 -0400

    Incorporate new CSS

--- frontend/build.sh
+++ frontend/build.sh
@@ -7,6 +7,10 @@ INDEXJSARR=(dist/assets/index-*.js)
 INDEXJS=${INDEXJSARR[0]}
 echo $INDEXJS
 
+INDEXCSSARR=(dist/assets/index-*.css)
+INDEXCSS=${INDEXCSSARR[0]}
+echo $INDEXCSS
+
 # --- Inline JSZip and the app bundle into the HTML ---
 
 echo '<script type="module">' > jszip.js
@@ -17,6 +21,11 @@ echo '<script type="module">' > indexjsrep.js
 cat "$INDEXJS" >> indexjsrep.js
 echo '</script>' >> indexjsrep.js
 
+echo '<style>' > indexcssrep.css
+cat "$INDEXCSS" >> indexcssrep.css
+echo '</style>' >> indexcssrep.css
+
+
 mv dist/index.html dist/index.html.bak
 sed '/script type="module" crossorigin src="\/assets\/index-/{
   r jszip.js
@@ -24,10 +33,15 @@ sed '/script type="module" crossorigin src="\/assets\/index-/{
   d
 }' dist/index.html.bak > dist/index.html.noicon
 
-sed '/link rel=/{
+sed '/favicon.ico/{
   r public/b64favicon
   d
-}' dist/index.html.noicon > dist/index.html
+}' dist/index.html.noicon > dist/index.html.nostyle
+
+sed '/stylesheet/{
+  r indexcssrep.css
+  d
+}' dist/index.html.nostyle > dist/index.html
 
 
-rm indexjsrep.js jszip.js
+rm indexcssrep.css indexjsrep.js jszip.js
